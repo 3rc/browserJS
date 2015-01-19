77582fd497d114feb8cdb819a3a3a688654afa94
3rc <thedj3rc@gmail.com>
2015-01-19 11:44:27 -0700

    Exercise files

---
+++ js2/main.js
@@ -0,0 +1,6 @@
+$(document).on('ready', function() {
+  
+	var word = prompt("Enter any word:");
+	alert("The word you entered was " + word + ", and the number of characters in it is " + word.length);
+
+});
