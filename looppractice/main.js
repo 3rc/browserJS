c6a34aefe970936b579b8aa2295ef520f8ba16c9
3rc <thedj3rc@gmail.com>
2015-01-19 12:38:45 -0700

    Loop Practice Completed

---
+++ looppractice/main.js
@@ -0,0 +1,50 @@
+$(document).on('ready', function() {
+
+/* COMPLETED PROBLEMS:
+
+ 	for(var i = 0; i <5; i++) {
+  	console.log(i);
+  } 
+
+for(var i = 0; i <= 500; i += 100) {
+	console.log(i);
+}
+
+for(var i = 1; i <= 64; i *= 2) {
+	console.log(i);
+}
+
+for(var i = 1; i < 4; i++) {
+	console.log(i);
+	console.log(i);
+	console.log(i);
+}
+
+for(var i = 0; i <= 10; i += 2) {
+	console.log(i);
+}
+
+for(var i = 3; i <= 15; i += 3) {
+	console.log(i);
+}
+
+for(var i = 9; i >= 0; i--) {
+	console.log(i)
+}
+
+*/
+
+	for(var i = 0; i < 4; i++) {
+		console.log(i)
+	}
+	for(var i = 0; i < 4; i++) {
+		console.log(i)
+	}
+	for(var i = 0; i < 4; i++) {
+		console.log(i)
+	}
+
+
+
+
+});
