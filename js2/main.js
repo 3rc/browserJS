159d97282c0bd7435e1d29923de50f9bb1d037af
3rc <thedj3rc@gmail.com>
2015-01-19 12:13:22 -0700

    js2 completed

--- js2/main.js
+++ js2/main.js
@@ -1,6 +1,11 @@
 $(document).on('ready', function() {
-  
-	var word = prompt("Enter any word:");
-	alert("The word you entered was " + word + ", and the number of characters in it is " + word.length);
 
+	var word = prompt("Enter any word:");
+	alert("The word you entered was:\n " + word);
+	alert("The number of characters in your word is:\n " + word.length);
+	alert("The third character of your word is:\n " + word[2]);
+	alert("Your word uppercase:\n " + word.toUpperCase());
+	alert("Your word lowercase:\n " + word.toLowerCase());
+	alert("In a sentence:\n Man, those are some really nice " + word + "s!");
+	alert("Subword:\n " + word[1] + word[2] + word[3]);
 });
