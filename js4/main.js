d1f6af11f2c381f88a7222b513601e20ae20c975
3rc <thedj3rc@gmail.com>
2015-01-21 16:48:08 -0700

    In progress

--- js4/main.js
+++ js4/main.js
@@ -1,33 +1,40 @@
 $(document).on('ready', function() {
 
 
-	var vicName = [];
-	var vicPhone = [];
-	var vicAddress = [];
-	var victims = [vicName, vicPhone, vicAddress];
+	var victims = [];
 	var i = 0;
   	var disaster = confirm("Enter Disaster Victim information?");
   	while(disaster == true) {
-  		var i = i + 1;
-		vicName.push(" " + prompt("Victim's name?"));
-		vicPhone.push(" " + prompt("Victim's phone number?"));
-		vicAddress.push(" " + prompt("Victim's address?"));
+  		
+  		victims[i] = {
+			vicName: (" " + prompt("Victim's name?")),
+			vicPhone: (" " + prompt("Victim's phone number?")),
+			vicAddress: (" " + prompt("Victim's address?"))
+			
+		}
+		var i = i + 1
 		var disaster = confirm("Enter another victim's information?");
-  }
+  	}
 
-	var volName = [];
-	var volPhone = [];
-	var volAddress = [];
-	var volunteers = [volName, volPhone, volAddress];
+	var volunteers = [];
 	var i = 0;
   	var disaster = confirm("Enter Disaster Volunteer information?");
   	while(disaster == true) {
-  		var i = i + 1;
-		volName.push(" " + prompt("Volunteer's name?"));
-		volPhone.push(" " + prompt("Volunteer's phone number?"));
-		volAddress.push(" " + prompt("Volunteer's address?"));
+		volunteers[i] = {
+			volName: (" " + prompt("Volunteer's name?")),
+			volPhone: (" " + prompt("Volunteer's phone number?")),
+			volAddress: (" " + prompt("Volunteer's address?"))
+			
+		}
+		var i = i + 1
 		var disaster = confirm("Enter another volunteer's information?");
 	}
-
-	alert("Victims' names: " + vicName)
+	var victimsAll = function() {
+		var victimNames = [];
+		for(var i = 0; i < victims.length; i++) {
+			victimNames.push(victims[i].vicName)
+		}
+		return victimNames
+	}
+	console.log(victimNames);
 });
---
+++ js5/main.js
@@ -0,0 +1,11 @@
+$(document).on('ready', function() {
+
+	$(".box").hover(function() {
+		$(this).toggleClass("editHover");
+	});
+  
+	$(".box").click(function() {
+		var txtEdit = '<div class="form '
+
+	}
+});
